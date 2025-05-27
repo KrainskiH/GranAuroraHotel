326991ef65dcf778df8a30c69e95d1a08aa56557
KrainskiH <henriquekrainski38@gmail.com>
2025-05-27 19:44:16 -0300

    Alterando

--- main/Main.java
+++ main/Main.java
@@ -1,5 +1,17 @@
+// Importa a classe Menu, que está na pasta 'view'
+
+import view.Menu;
 package main;
+// Classe principal do programa
 public class Main {
 
-    
+    // Métotodo principal onde o programa inicia
+    public static void main (String[] args){
+
+        // Cria um objeto da classe Menu
+        Menu menu = new Menu();
+
+        // Chama o metodo que exibe o menu e permite a interação com o usuario
+        menu.exibirMenu();
+    }
 }
