a74e2f54aa0134ce7614806951ab326b576a4f2c
VickAzevedo <azevedovictoria49@gmail.com>
2025-06-08 21:16:05 -0300

    Criar QuartoService.java

---
+++ service/QuartoService.java
@@ -0,0 +1,43 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import model.Quarto;
+import model.QuartoLuxo;
+import model.QuartoSimples;
+import model.QuartoStandard;
+
+public class QuartoService {
+
+    private List<Quarto> quartos = new ArrayList<>();
+
+    
+    public QuartoService() {
+        
+        quartos.add(new QuartoSimples(101));
+        quartos.add(new QuartoSimples(102));
+        quartos.add(new QuartoStandard(201));
+        quartos.add(new QuartoStandard(202));
+        quartos.add(new QuartoLuxo(301));
+    }
+
+    public List<Quarto> listarTodosOsQuartos() {
+        return quartos;
+    }
+
+    public List<Quarto> listarQuartosDisponiveis() {
+        return quartos.stream()
+                      .filter(q -> !q.isOcupado())
+                      .collect(Collectors.toList());
+    }
+
+    public Quarto buscarQuartoPorNumero(int numero) {
+        for (Quarto quarto : quartos) {
+            if (quarto.getNumero() == numero) {
+                return quarto;
+            }
+        }
+        return null; // Retorna null se não encontrar
+    }
+}
