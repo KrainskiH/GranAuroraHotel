d8a4801e3edef79eb01d1be54ba5e5d27f33191a
VickAzevedo <azevedovictoria49@gmail.com>
2025-06-08 21:12:20 -0300

    Atualizar o QuartoLuxo.java

--- model/QuartoLuxo.java
+++ model/QuartoLuxo.java
@@ -1,5 +1,25 @@
 package model;
 
-public enum QuartoLuxo {
-    
+public class QuartoLuxo extends Quarto {
+
+    private static final double PRECO_DIARIA = 450.00;
+
+    public QuartoLuxo(int numero) {
+        super(numero);
+    }
+
+    @Override
+    public String getTipo() {
+        return "Luxo";
+    }
+
+    @Override
+    public String getDescricao() {
+        return "Suíte de luxo com cama king-size, ar-condicionado, TV 50\", frigobar e banheira de hidromassagem.";
+    }
+
+    @Override
+    public double getPrecoDiaria() {
+        return PRECO_DIARIA;
+    }
 }
