874d4d6f10dd6989fd062bde5c07247fc1ec8431
VickAzevedo <azevedovictoria49@gmail.com>
2025-06-08 21:14:25 -0300

    Criar QuartoSimples.java

---
+++ model/QuartoSimples.java
@@ -0,0 +1,25 @@
+package model;
+
+public class QuartoSimples extends Quarto {
+
+    private static final double PRECO_DIARIA = 120.00;
+
+    public QuartoSimples(int numero) {
+        super(numero); // Chama o construtor da classe mãe (Quarto)
+    }
+
+    @Override
+    public String getTipo() {
+        return "Simples";
+    }
+
+    @Override
+    public String getDescricao() {
+        return "Quarto simples com uma cama de solteiro, ventilador e banheiro compartilhado no corredor.";
+    }
+
+    @Override
+    public double getPrecoDiaria() {
+        return PRECO_DIARIA;
+    }
+}
