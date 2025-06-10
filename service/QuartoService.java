1f4609d00f59e4bcaff31971508bfd0bb47ae283
VickAzevedo <azevedovictoria49@gmail.com>
2025-06-10 19:05:34 -0300

    QuartoService.java

--- service/QuartoService.java
+++ service/QuartoService.java
@@ -9,35 +9,29 @@ import model.QuartoSimples;
 import model.QuartoStandard;
 
 public class QuartoService {
-
     private List<Quarto> quartos = new ArrayList<>();
-
-    
-    public QuartoService() {
-        
-        quartos.add(new QuartoSimples(101));
-        quartos.add(new QuartoSimples(102));
-        quartos.add(new QuartoStandard(201));
-        quartos.add(new QuartoStandard(202));
-        quartos.add(new QuartoLuxo(301));
+public QuartoService() {
+    quartos.add(new QuartoSimples(101));
+    quartos.add(new QuartoSimples(102));
+    quartos.add(new QuartoStandard(201));
+    quartos.add(new QuartoStandard(202));
+    quartos.add(new QuartoLuxo(301));
+    }
+public List<Quarto> listarTodosOsQuartos() {
+    return quartos;
+    }
+public List<Quarto> listarQuartosDisponiveis() {
+    return quartos.stream()
+        .filter(q -> !q.isOcupado())
+        .collect(Collectors.toList());
     }
 
-    public List<Quarto> listarTodosOsQuartos() {
-        return quartos;
-    }
-
-    public List<Quarto> listarQuartosDisponiveis() {
-        return quartos.stream()
-                      .filter(q -> !q.isOcupado())
-                      .collect(Collectors.toList());
-    }
-
-    public Quarto buscarQuartoPorNumero(int numero) {
-        for (Quarto quarto : quartos) {
-            if (quarto.getNumero() == numero) {
-                return quarto;
+public Quarto buscarQuartoPorNumero(int numero) {
+    for (Quarto quarto : quartos) {
+    if (quarto.getNumero() == numero) {
+    return quarto;
             }
         }
-        return null; // Retorna null se não encontrar
+    return null; // Retorna null se não encontrar
     }
 }
