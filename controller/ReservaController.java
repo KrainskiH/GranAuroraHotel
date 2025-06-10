f91cd4038ef694449e3823ac9d3ac493daf7ddfc
Krainski <henriquekrainski38@gmail.com>
2025-06-10 20:58:34 -0300

    Finalizando controller

--- controller/ReservaController.java
+++ controller/ReservaController.java
@@ -2,11 +2,70 @@ package controller;
 
 import model.Reserva;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ReservaController {
     private ArrayList<Reserva> reservas = new ArrayList<>();
     private int proximoId = 1;
 
+    public void gerenciarReservas(Scanner scanner) {
+    int opcao;
+    do {
+        System.out.println("\n--- Menu de Reservas ---");
+        System.out.println("1 - Cadastrar reserva");
+        System.out.println("2 - Listar reservas");
+        System.out.println("3 - Editar reserva");
+        System.out.println("4 - Excluir reserva");
+        System.out.println("0 - Voltar");
+        System.out.print("Escolha uma opção: ");
+        
+        opcao = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (opcao) {
+            case 1:
+                System.out.print("ID do cliente: ");
+                int clienteId = scanner.nextInt();
+                System.out.print("ID do quarto: ");
+                int quartoId = scanner.nextInt();
+                scanner.nextLine();
+                System.out.print("Data de entrada: ");
+                String entrada = scanner.nextLine();
+                System.out.print("Data de saída: ");
+                String saida = scanner.nextLine();
+                cadastrarReserva(clienteId, quartoId, entrada, saida);
+                break;
+            case 2:
+                listarReservas();
+                break;
+            case 3:
+                System.out.print("ID da reserva: ");
+                int idEditar = scanner.nextInt();
+                System.out.print("Novo ID do cliente: ");
+                int novoCliente = scanner.nextInt();
+                System.out.print("Novo ID do quarto: ");
+                int novoQuarto = scanner.nextInt();
+                scanner.nextLine();
+                System.out.print("Nova data de entrada: ");
+                String novaEntrada = scanner.nextLine();
+                System.out.print("Nova data de saída: ");
+                String novaSaida = scanner.nextLine();
+                editarReserva(idEditar, novoCliente, novoQuarto, novaEntrada, novaSaida);
+                break;
+            case 4:
+                System.out.print("ID da reserva a excluir: ");
+                int idExcluir = scanner.nextInt();
+                excluirReserva(idExcluir);
+                break;
+            case 0:
+                System.out.println("Voltando ao menu principal...");
+                break;
+            default:
+                System.out.println("Opção inválida.");
+        }
+
+    } while (opcao != 0);
+}
     public void cadastrarReserva(int idCliente, int idQuarto, String entrada, String saida) {
         Reserva r = new Reserva(proximoId, idCliente, idQuarto, entrada, saida);
         reservas.add(r);
