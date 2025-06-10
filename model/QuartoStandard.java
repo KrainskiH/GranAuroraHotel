92a606888def3c124283d0bb11f3fcaca64c129f
VickAzevedo <azevedovictoria49@gmail.com>
2025-06-10 19:02:36 -0300

    QuartoStandard.java

--- model/QuartoStandard.java
+++ model/QuartoStandard.java
@@ -1,23 +1,18 @@
 package model;
 
 public class QuartoStandard extends Quarto {
-
     private static final double PRECO_DIARIA = 200.50;
-
     public QuartoStandard(int numero) {
-        super(numero);
+    super(numero);
     }
-
     @Override
     public String getTipo() {
         return "Standard";
     }
-
     @Override
     public String getDescricao() {
-        return "Quarto standard com uma cama de casal, ar-condicionado, TV e banheiro privativo.";
+        return "Quarto standard com uma cama de casal, ar-condicionado, TV, mobilia aconchegante e banheiro confortável privativo.";
     }
-
     @Override
     public double getPrecoDiaria() {
         return PRECO_DIARIA;
