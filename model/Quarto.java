4a4dd2c53350f777671238ab1974d85a334cb632
Krainski <henriquekrainski38@gmail.com>
2025-06-10 20:16:39 -0300

    Alterando Quarto

--- model/Quarto.java
+++ model/Quarto.java
@@ -1,39 +1,31 @@
 package model;
 
 public abstract class Quarto {
-    
-    protected int numero; // 'protected' significa que as classes filhas podem acessar esta variável
 
-    /**
-     * Construtor que toda classe filha precisará chamar.
-     * @param numero O número de identificação do quarto.
-     */
+    protected int numero;
+    protected boolean ocupado = false;
+
     public Quarto(int numero) {
         this.numero = numero;
     }
 
-    /**
-     * Retorna o número do quarto.
-     */
     public int getNumero() {
         return numero;
     }
 
-    // MÉTODOS ABSTRATOS
-    // Toda classe que herdar de Quarto SERÁ OBRIGADA a implementar estes métodos.
+    public boolean isOcupado() {
+        return ocupado;
+    }
 
-    /**
-     * Retorna o tipo do quarto (ex: "Luxo", "Simples").
-     */
+    public void ocupar() {
+        this.ocupado = true;
+    }
+
+    public void desocupar() {
+        this.ocupado = false;
+    }
+
     public abstract String getTipo();
-
-    /**
-     * Retorna a descrição detalhada do quarto.
-     */
     public abstract String getDescricao();
-
-    /**
-     * Retorna o preço da diária do quarto.
-     */
     public abstract double getPrecoDiaria();
 }
