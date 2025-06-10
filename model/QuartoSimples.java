eb53b3e524fddbecd13d45c7289bc60e84355da2
VickAzevedo <azevedovictoria49@gmail.com>
2025-06-10 18:56:19 -0300

    QuartoSimples.java

--- model/QuartoSimples.java
+++ model/QuartoSimples.java
@@ -1,23 +1,18 @@
 package model;
 
 public class QuartoSimples extends Quarto {
-
-    private static final double PRECO_DIARIA = 120.00;
-
-    public QuartoSimples(int numero) {
-        super(numero); // Chama o construtor da classe mãe (Quarto)
+  private static final double PRECO_DIARIA = 120.00;
+public QuartoSimples(int numero) {
+  super(numero); // Chama o construtor da classe mãe (Quarto)
     }
-
     @Override
     public String getTipo() {
         return "Simples";
     }
-
     @Override
     public String getDescricao() {
-        return "Quarto simples com uma cama de solteiro, ventilador e banheiro compartilhado no corredor.";
+        return "Quarto simples com uma cama de solteiro, ventilador, mobilia básica e banheiro compartilhado no corredor.";
     }
-
     @Override
     public double getPrecoDiaria() {
         return PRECO_DIARIA;
