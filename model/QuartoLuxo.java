da7d07e727a2e3089c69f7c823ef57bbb56884c8
VickAzevedo <azevedovictoria49@gmail.com>
2025-06-10 18:54:29 -0300

    QuartoLuxo.java

--- model/QuartoLuxo.java
+++ model/QuartoLuxo.java
@@ -1,23 +1,18 @@
 package model;
 
 public class QuartoLuxo extends Quarto {
-
-    private static final double PRECO_DIARIA = 450.00;
-
-    public QuartoLuxo(int numero) {
-        super(numero);
+   private static final double PRECO_DIARIA = 450.00;
+public QuartoLuxo(int numero) {
+    super(numero);
     }
-
     @Override
     public String getTipo() {
         return "Luxo";
     }
-
     @Override
     public String getDescricao() {
         return "Suíte de luxo com cama king-size, ar-condicionado, TV 50\", frigobar e banheira de hidromassagem.";
     }
-
     @Override
     public double getPrecoDiaria() {
         return PRECO_DIARIA;
