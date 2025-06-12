94d0567f6bc48e1cd30415f7039f1acf25b07d7c
CarlaPrislanny <prislannysilva@gmail.com>
2025-06-12 12:05:42 -0300

    FEITO!

--- utils/Log.java
+++ utils/Log.java
@@ -1,5 +1,22 @@
 package utils;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Log {
-    
+
+    private static final String LOG_FILE = "log.txt";
+
+    public static void registrar(String mensagem) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
+            String dataHora = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+            writer.write("[" + dataHora + "] " + mensagem);
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Erro ao escrever no log: " + e.getMessage());
+        }
+    }
 }
