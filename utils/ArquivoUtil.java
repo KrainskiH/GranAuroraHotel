bee363c9938edd148f54ef0850e3b0b4a1f992ad
CarlaPrislanny <prislannysilva@gmail.com>
2025-06-12 13:26:51 -0300

    FEITO!

--- utils/ArquivoUtil.java
+++ utils/ArquivoUtil.java
@@ -1,5 +1,24 @@
 package utils;
 
+import model.Reserva;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class ArquivoUtil {
-    
+
+    private static final String ARQUIVO_RESERVAS = "reservas.txt";
+
+    public static void salvarReservas(List<Reserva> reservas) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(ARQUIVO_RESERVAS))) {
+            for (Reserva r : reservas) {
+                writer.write(r.getId() + ";" + r.getIdCliente() + ";" + r.getIdQuarto() + ";" +
+                             r.getDataEntrada() + ";" + r.getDataSaida());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao salvar reservas: " + e.getMessage());
+        }
+    }
 }
